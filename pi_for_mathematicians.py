b5feeda13c17fa131b3a1a355f5dc0c040e35816
Louis Steele <louis.g.steele@gmail.com>
2016-05-01 17:36:05 -0700

    added pi_for_mathematicians

---
+++ pi_for_mathematicians.py
@@ -0,0 +1,1 @@
+#pi_for_mathematicians.
--- pi_for_physicists.py
+++ pi_for_physicists.py
@@ -1,5 +1,5 @@
 #This is a program that determines if you know the value of pi.  It assumes you are a physicist.
-# To run (F5) enter the following command C:\Python27\Lib\idlelib\idle.py "$(FULL_CURRENT_PATH)"
+# To run in Notepad ++ type F5, then enter the following command C:\Python27\Lib\idlelib\idle.py "$(FULL_CURRENT_PATH)"
 
 def pi():
 	a = raw_input("You are a physicist, eh?  What is the value of pi?\n")
